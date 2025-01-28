582210072c1a2dd03f0707b7f3ef237148b1873d
Joshua Tenorio <joshliam09@gmail.com>
2025-01-27 20:53:19 -0700

    dont hardcode websocket endpoint url

--- src/app/chat/[chatid]/layout.tsx
+++ src/app/chat/[chatid]/layout.tsx
@@ -1,11 +1,12 @@
 'use client'
 import React from 'react'
 import { WebSocketProvider } from 'next-ws/client'
+import { env } from "~/env"
 
 function ChatLayout({ children }: React.PropsWithChildren) {
   return (
     <div>
-        <WebSocketProvider url='ws://localhost:3000/api/socket'>
+        <WebSocketProvider url={env.NEXT_PUBLIC_WS_ENDPOINT}>
             {children}
         </WebSocketProvider>
     </div>
