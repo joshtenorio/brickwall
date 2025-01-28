2ec6605e500d8be710b330d8312b1809ee726011
Joshua Tenorio <joshliam09@gmail.com>
2025-01-27 20:29:35 -0700

    fix spelling in eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,7 @@ const config = {
   ],
   "rules": {
     "@typescript-eslint/no-misused-promises": "warn",
-    "@typescript-eslitn/no-unsafe-assignment": "warn",
+    "@typescript-eslint/no-unsafe-assignment": "warn",
     "@typescript-eslint/no-explicit-any": "warn",
     "@typescript-eslint/no-unsafe-member-access": "warn",
     "@typescript-eslint/array-type": "off",
